d86f61692db114d722050bbf96b4a0f53133f2a1
Sao I Kuan <saoikuan@gmail.com>
2022-09-22 02:03:29 -0400

    update

--- app/src/main/java/com/example/formapp/NextActivity.kt
+++ app/src/main/java/com/example/formapp/NextActivity.kt
@@ -12,7 +12,16 @@ class NextActivity : AppCompatActivity() {
         val intent=intent
         val message=intent.getStringExtra("Message")
 
-        val textView=findViewById<TextView>(R.id.textView)
-        textView.text=message
+        val textView1=findViewById<TextView>(R.id.textView1)
+        textView1.text=message
+        val textView2=findViewById<TextView>(R.id.textView2)
+        textView2.text=message
+        val textView3=findViewById<TextView>(R.id.textView3)
+        textView3.text=message
+        val textView4=findViewById<TextView>(R.id.textView4)
+        textView4.text=message
+        val textView5=findViewById<TextView>(R.id.textView5)
+        textView5.text=message
+
     }
 }
